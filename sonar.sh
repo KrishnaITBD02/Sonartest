09e4abdadb9f73d725748c664e709af194cb00a0
Krishna Sharma <124158389+KrishnaITBD02@users.noreply.github.com>
2023-04-10 12:57:49 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -2,4 +2,4 @@ sonar-scanner \
   -Dsonar.projectKey=TESTNEW \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=sqp_bdd5744dab0eedec6111054fd859efd7287d9dc9
+  -Dsonar.token=sqp_01027cbc7c995326324867e101aee0a0330ef17c
